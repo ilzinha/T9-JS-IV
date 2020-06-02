a52fedaa80578ecb6c9628908e9c422ccdb9efc9
Ilza <ilzammed2@gmail.com>
2020-06-02 09:46:55 -0300

    resuluçao doo exercicio de segunda

--- revisao/objetos.js
+++ revisao/objetos.js
@@ -4,9 +4,9 @@ const pessoa = {
 }
 pessoa.personalidade
 
-console.log(typeof(pessoa))
-  personalidade: ['personalide1', 'personalide2'],
-  areaAtuacao: 'Biologa'
+console.log(typeof (pessoa))
+personalidade: ['personalide1', 'personalide2'],
+    areaAtuacao: 'Biologa'
 }
 
 pessoa.personalidade[0];
@@ -17,5 +17,36 @@ console.log("Personalidade1: ", pessoa.personalidade[0]);
 //exercício
 
 
+const cliente1 = {
+    nome: 'Dora Barreto',
+    cpf: 22300122129,
+    valorCompra: 2000,
+    modeloCelular: 'XXX-12',
+    anoCelular: 2020,
+    corCelular: 'Amarelo',
+    loja: 'RJ-BR',
+    atendente: 'Joana Maria'
+}
 
+const cliente2 = {
+    nome: 'Marcela Janino Silva',
+    cpf: 32110123129,
+    valorCompra: 1300,
+    modeloCelular: 'XXM - y',
+    anoCelular: 2019,
+    corCelular: 'ROXO',
+    loja: 'SP - BR',
+    atendente: 'Drielly Gomes Almeida'
+}
 
+const cliente3 = {
+    nome: 'Raquel Borges Figueredo',
+    cpf: 91031142501,
+    valorCompra: 4000,
+    modeloCelular: 'MDM - PRO',
+    anoCelular: 2020,
+    corCelular: 'AZUL',
+    loja: 'MG - BR',
+    atendente: 'Fran Carneiro de Pinha'
+
+}
