3c725f807a83562b56ace157fc31e79b11fd4397
Ilza <ilzammed2@gmail.com>
2020-06-02 11:28:44 -0300

    Resoluçao dos exercicios

--- revisao/exercicios_objeto/analise_objetos.js
+++ revisao/exercicios_objeto/analise_objetos.js
@@ -1,15 +1,38 @@
 //Dado o objeto, analise a forma que foi declarada. 
 //Em seguida, ajuste o objeto produtos da forma correta de se declarar.
 let produtos = {
+
   videos: {
-    comedia: ["comedia1""comedia2"];
-    romance ["romance1", "romance2"]
+    comedia: ["comedia1","comedia2"],
+    romance: ["romance1", "romance2"]
   },
   revistas: [
     moda = ["lalala", "lililili"],
     saude = ["lalala", "lililili"],
-  ]
-  jogos {
-    rpg ["rpg1", "rpg2", rpg3],
+  ],
+
+  jogos :{
+    rpg: ["rpg1", "rpg2", "rpg3"],
     acao: ["acao1", "God of War"]
   }
+
+}
+
+//criando um objeto de forma nativa:
+const minhaInfos = new Object();
+
+//Adicionando objetos
+minhaInfos.nome = 'Ilza';
+minhaInfos.sobrenome = 'Medeiros';
+minhaInfos.idade = 32;
+minhaInfos.filmes =  ['Harry Potter', 'Star Wars', 'Pride and Prejudice']
+
+// Seria a mesma coisa se eu declarasse assim:
+// const minhaInfos = {
+//   nome: 'Ilza',
+//   sobrenome: 'Medeiros',
+//   idade: 32,
+//   filmes: ['Harry Potter', 'Star Wars', 'Pride and Prejudice']
+// }
+
+console.log(minhaInfos)
